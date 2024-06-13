c00a6786b48493db74d53f3fafd9f4b9402dc82f
d-sofiika <soniadrozd2709@gmail.com>
2024-06-13 19:46:33 +0200

    update onClick for modal

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,17 +1,21 @@
-import css from "./imageCard.module.css"
+import css from "./imageCard.module.css";
 
-const ImageCard = ({ url, dscr }) => {
-     const capitalizeFirstLetter = (text) => {
+const ImageCard = ({ url, dscr, onImageClick }) => {
+  const capitalizeFirstLetter = (text) => {
     if (text.length === 0) return text;
     return text.charAt(0).toUpperCase() + text.slice(1);
   };
   return (
     <div>
-          <img className={css.img} src={url} alt={dscr} />
-          <p className={css.dscr} >{capitalizeFirstLetter(dscr)}</p>
-</div>
+      <img
+        className={css.img}
+        src={url}
+        alt={dscr}
+        onClick={ onImageClick }
+      />
+      <p className={css.dscr}>{capitalizeFirstLetter(dscr)}</p>
+    </div>
+  );
+};
 
-  )
-}
-
-export default ImageCard
+export default ImageCard;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,14 @@
 import ImageCard from "../ImageCard/ImageCard";
 import css from "./imageGallery.module.css"
 
-const ImageGallery = ({ images, onImageClick }) => {
+const ImageGallery = ({ images, onImageClick}) => {
   return (
     <ul className={css.listGallery} >
           {images.map((img) => (
           
-            <li className={css.item} key={img.id} onClick={() => onImageClick(img.urls.regular, img.likes, img.alt_description)}>
-          <ImageCard url={img.urls.small} dscr={img.alt_description
-} />
+            <li className={css.item} key={img.id}>
+              <ImageCard url={img.urls.small} dscr={img.alt_description
+              } onImageClick={() => onImageClick(img.urls.regular, img.likes, img.alt_description)} />
         </li>
       ))}
     </ul>
